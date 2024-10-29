74a61303e5e25fba0e9c4a529929c75956f7a595
Om Satam <omsatam2268@gmail.com>
2024-10-29 19:00:25 -0400

    Initial commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: Password, required: true }
+    password: { type: String, required: true }
 });
 console.log('Data saved successfully!');
 
